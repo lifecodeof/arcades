5745a293f6cbdc1f70b772326220fbd05ef35cbb
lifecodeof <42251538+lifecodeof@users.noreply.github.com>
2022-07-20 14:35:27 +0300

    correct & improve InfoCard

--- apps/web/src/components/InfoCard.tsx
+++ apps/web/src/components/InfoCard.tsx
@@ -1,6 +1,8 @@
 import { FC, useEffect, useState } from "react"
-import { Paper, Typography } from "@mui/material"
+import { Paper, Typography, Link } from "@mui/material"
 import dapp from "../dapp"
+
+const scanUrl = "https://testnet.bscscan.com/address/"
 
 const AddressInfoCard: FC = () => {
     return (
@@ -8,8 +10,8 @@ const AddressInfoCard: FC = () => {
             <Typography variant="h5" component="div" color="text.secondary">
                 Contract addresses
             </Typography>
-            <Typography variant="body2">Arcades (ARC): {dapp.arcades.address}</Typography>
-            <Typography variant="body2">Scrap Token (SCRAP): {dapp.arcades.address}</Typography>
+            <Typography variant="body2">Arcades (ARC): <Link target="_blank" rel="noopener" href={scanUrl + dapp.arcades.address}>{dapp.arcades.address}</Link></Typography>
+            <Typography variant="body2">Scrap Token (SCRAP): <Link target="_blank" rel="noopener" href={scanUrl + dapp.scrapToken.address}>{dapp.scrapToken.address}</Link></Typography>
         </Paper>
     )
 }
